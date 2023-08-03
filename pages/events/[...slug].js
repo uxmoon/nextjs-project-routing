67cb2f048cd4d4b662d6d062154c1d341c4aeba3
uxmoon <uxmoon@gmail.com>
2023-08-03 16:01:01 -0300

    Build routes for events

---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+export default function FilteredEventsPage() {
+  return (
+    <>
+      <h1>Filtered Events Page</h1>
+    </>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventsPage() {
+  return (
+    <>
+      <h1>EventsPage</h1>
+    </>
+  );
+}
