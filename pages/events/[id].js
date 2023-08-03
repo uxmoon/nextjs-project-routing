ca4abd01d3aa5b9dc825d393e3be75191745e3b3
uxmoon <uxmoon@gmail.com>
2023-08-03 18:22:13 -0300

    Display event detail information

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,7 +1,28 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '@/dummy-data';
+import EventSummary from '@/components/event-detail/EventSummary';
+import EventLogistics from '@/components/event-detail/EventLogistics';
+import EventContent from '@/components/event-detail/EventContent';
+
 export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No events found</p>;
+  }
   return (
     <>
-      <h1>Event Detail Page</h1>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
     </>
   );
 }
