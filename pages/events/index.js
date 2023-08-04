854cc6f2bb35db73e2744c00bd8c4365362f7c67
uxmoon <uxmoon@gmail.com>
2023-08-04 12:17:06 -0300

    Add search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,13 @@
+import EventList from '@/components/events/EventList';
+import EventsSearch from '@/components/events/EventsSearch';
+import { getAllEvents } from '@/dummy-data';
+
 export default function EventsPage() {
+  const events = getAllEvents();
   return (
     <>
-      <h1>EventsPage</h1>
+      <EventsSearch />
+      <EventList items={events} />
     </>
   );
 }
